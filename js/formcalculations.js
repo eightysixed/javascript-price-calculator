409c40716fc7d2cab3a332a426c0aa9a830ddfbf
Kevin <kevin@vnucleus.com>
2019-02-27 08:55:12 -0500

    Update formcalculations.js

--- js/formcalculations.js
+++ js/formcalculations.js
@@ -7,11 +7,11 @@ You are free to use the code in Commercial or Non-Commercial projects
 
 function getQuantity()
 {
-    //Assume form with id="theform"
+    //Assume form with id="theForm"
     var theForm = document.forms["calculator"];
     //Get a reference to the TextBox
     var quantity = theForm.elements["quantity"];
-    var howmany =1;
+    var howmany = 1;
     //If the TextBox is not blank
     if(quantity.value!="")
     {
@@ -24,13 +24,12 @@ function calculateTotal()
 {
     //Get Total Price by Calling the Function
     var widgetPrice = getQuantity()*1.49; //Change 1.49 to Any Multiplier
-    var widgetPrice2 = Math.round(widgetPrice*100)/100  //Round the Result to 2 Decimal Points
+    var widgetPriceRounded = Math.round(widgetPrice*100)/100  //Round the Result to 2 Decimal Points
 
     //Display the Result
     var divobj = document.getElementById('totalPrice');
-    divobj.style.display='block';
-    divobj.innerHTML = "Total Price: $"+widgetPrice2;
-
+    divobj.style.display = 'block';
+    divobj.innerHTML = "Total Price: $"+widgetPriceRounded;
 }
 
 function hideTotal()
